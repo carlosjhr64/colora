875dbfee385a61e88af881fd74693c0401fa4411
CarlosJHR64 <carlosjhr64@gmail.com>
2024-01-13 07:51:03 -0800

    Added configure.rb

--- lib/colora.rb
+++ lib/colora.rb
@@ -2,55 +2,19 @@ module Colora
   class Error < RuntimeError; end
   VERSION = '0.1.240113'
 
-  # Colora.filter
-  def self.filter = @filter
-  def self.filter=(filter)
-    @filter=filter
-  end
-  Colora.filter = ''
-
-  # Colora.file
-  def self.file = @file
-  def self.file=(file)
-    @file=file
-  end
-  Colora.file = nil
-
-  # Colora.git?
-  def self.git? = @git
-  def self.git=(git)
-    @git = !!git
-  end
-  Colora.git = false
-
-  # Colora.lang
-  def self.lang = @lang
-  def self.lang=(lang)
-    @lang=lang
-  end
-  Colora.lang = 'ruby'
-
-  # Colora.theme
-  def self.theme = @theme
-  def self.theme=(theme)
-    @theme=theme
-  end
-  Colora.theme = 'github'
-
-  # Colora.tab?
-  def self.tab? = @tab
-  def self.tab=(tab)
-    @tab = !!tab
-  end
-  Colora.tab = false
-
-  # Colora.run
-  def self.run
+  # Colora.run(options)
+  def self.run(options=nil)
     require 'fuzzystringmatch'
     require 'rainbow/refinement'
     require 'rouge'
-    require 'colora/data'
-    require 'colora/lines'
+    require_relative 'colora/configure'
+    require_relative 'colora/data'
+    require_relative 'colora/lines'
+
+    Colora.configure(options) if options
+    # By default, run git-diff:
+    Colora.git = true if $stdin.tty? && !Colora.file
+
     Lines.new.each do |line|
       line.gsub!("\t", '⇥') if Colora.tab?
       puts line
---
+++ lib/colora/configure.rb
@@ -0,0 +1,59 @@
+module Colora
+  # Colora.filter
+  def self.filter = @filter
+  def self.filter=(filter)
+    @filter=filter
+  end
+  Colora.filter = ''
+
+  # Colora.file
+  def self.file = @file
+  def self.file=(file)
+    @file=file
+  end
+  Colora.file = nil
+
+  # Colora.git?
+  def self.git? = @git
+  def self.git=(git)
+    @git = !!git
+  end
+  Colora.git = false
+
+  # Colora.lang
+  def self.lang = @lang
+  def self.lang=(lang)
+    @lang=lang
+  end
+  Colora.lang = 'ruby'
+
+  # Colora.theme
+  def self.theme = @theme
+  def self.theme=(theme)
+    @theme=theme
+  end
+  Colora.theme = 'github'
+
+  # Colora.tab?
+  def self.tab? = @tab
+  def self.tab=(tab)
+    @tab = !!tab
+  end
+  Colora.tab = false
+
+  def self.configure(options)
+    # FILE:
+    Colora.file = options.file if options.file
+    # Options:
+    Colora.filter << 'q' if options.quiet?
+    Colora.filter << '+' if options.green?
+    Colora.filter << '-' if options.red?
+    Colora.filter << 'c' if options.code?
+    Colora.filter << 'd' if options.dup?
+    Colora.filter << 'C' if options.comment?
+    Colora.theme = options.theme if options.theme?
+    Colora.lang = options.lang if options.lang?
+    Colora.git = options.git?
+    Colora.tab = options.tab?
+  end
+end
