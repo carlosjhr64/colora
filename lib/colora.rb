2572f6ce33f89dd9becb2aec0f429d3504e0778f
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-27 08:51:34 -0700

    review bin, colora.rb, added requires.rb.

--- lib/colora.rb
+++ lib/colora.rb
@@ -1,32 +1,28 @@
+# frozen_string_literal: true
+
+# Colora colorizes output to the terminal.
 module Colora
-  class Error < RuntimeError; end
   VERSION = '0.2.250327'
 
+  # Colora::Error for raised exceptions in Colora.
+  # Anything else is a bug.
+  class Error < RuntimeError
+  end
+
+  def self.parse_argv(version, help)
+    require 'help_parser'
+    HelpParser[version, help]
+  end
+
   # Colora.run(options)
-  def self.run(options=nil)
-    # Standard libraries:
-    require 'ostruct'
-    # Gems:
-    require 'fuzzystringmatch'
-    require 'paint'
-    require 'rouge'
-    # Colora:
-    require_relative 'colora/configure'
-    require_relative 'colora/data'
-    require_relative 'colora/lines'
-    # Plugs:
-    require_relative 'colora/plugs/diff'
-    require_relative 'colora/plugs/markdown'
-
-    # Configure Colora:
-    Colora.configure(options) if options
-    # By default, run git-diff:
-    Config.git = true if $stdin.tty? && !Config.file
-
-    # Puts Colora::Lines
+  # :reek:TooManyStatements
+  def self.run(options = nil)
+    require_relative 'colora/requires'
+    Colora.configure(options) if options # Configure Colora:
+    Config.git = true if $stdin.tty? && !Config.file # By default, run git-diff
     Lines.new.each do |line|
       line.gsub!("\t", '⇥') if Config.tab
-      puts line
+      puts line # Puts Colora::Lines
     end
   end
 end
---
+++ lib/colora/requires.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+# Standard libraries:
+require 'ostruct'
+# Gems:
+require 'fuzzystringmatch'
+require 'paint'
+require 'rouge'
+# Colora:
+require_relative 'configure'
+require_relative 'data'
+require_relative 'lines'
+# Plugs:
+require_relative 'plugs/diff'
+require_relative 'plugs/markdown'
