4a9e9940d30f3006219c52a507ed9c17a1f4ee02
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-27 10:25:27 -0700

    Rubocop autocorrect data.rb

--- lib/colora/data.rb
+++ lib/colora/data.rb
@@ -1,20 +1,24 @@
+# frozen_string_literal: true
+
+# Colora namespace.
 module Colora
+  # Data class for processing git-diff lines.
   class Data
     SPLIT = lambda do |line|
       flag = line[0]
       code, pounds, comment = line[1..].split(/(?<!['"])(\s*#+)(?!{)/, 2)
       code = nil if code.empty?
-      comment ? [flag, code, pounds+comment] : [flag, code, nil]
+      comment ? [flag, code, pounds + comment] : [flag, code, nil]
     end
     UPDATE = lambda do |hash, key, flag|
       k = key.strip
       hash[k] = case hash[k]
                 when nil
-                    flag # added(+>) or removed(-<)
+                  flag # added(+>) or removed(-<)
                 when 'd', 't', flag
-                    'd' # duplicate
+                  'd' # duplicate
                 else
-                    't' # touched
+                  't' # touched
                 end
     end
 
@@ -25,7 +29,11 @@ module Colora
         line
       when /^[-+<>]/
         flag, code, comment = SPLIT[line]
-        f = flag=='-' ? '<' : flag=='+' ? '>' : flag
+        f = if flag == '-'
+              '<'
+            else
+              flag == '+' ? '>' : flag
+            end
         UPDATE[@codes, code, f] if code
         UPDATE[@comments, comment, f] if comment
         [flag, code, comment]
@@ -38,13 +46,17 @@ module Colora
     attr_reader :lines
 
     def initialize(lines)
-      @lines,@codes,@comments,@edits = [],{},{},Set.new
+      @lines = []
+      @codes = {}
+      @comments = {}
+      @edits = Set.new
       while (line = lines.shift)
         @lines << pre_process(line)
       end
       populate_edits
       @lines.each do |line|
         next unless line.is_a?(Array)
+
         post_process line
       end
       @codes = @comments = @edits = nil # GC
@@ -53,8 +65,8 @@ module Colora
     def populate_edits
       partners = []
       jarrow = FuzzyStringMatch::JaroWinkler.create(:pure) # Need pure for UTF-8
-      removed = @codes.select{|_,flag| '-<'.include?flag}.keys
-      added   = @codes.select{|_,flag| '+>'.include?flag}.keys
+      removed = @codes.select { |_, flag| '-<'.include? flag }.keys
+      added   = @codes.select { |_, flag| '+>'.include? flag }.keys
       short, long = [removed, added].sort_by(&:length)
       short.each do |a|
         long.each do |b|
@@ -64,6 +76,7 @@ module Colora
       end
       partners.sort_by(&:last).reverse.each do |a, b, _|
         next if @edits.include?(a) || @edits.include?(b)
+
         @edits.add(a)
         @edits.add(b)
       end
