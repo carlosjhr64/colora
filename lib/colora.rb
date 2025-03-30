441c4e710e535389da2803620a07c9173d2af180
CarlosJHR64 <carlosjhr64@gmail.com>
2025-03-30 09:54:11 -0700

    Bumip

--- lib/colora.rb
+++ lib/colora.rb
@@ -2,7 +2,7 @@
 
 # Colora colorizes output to the terminal.
 module Colora
-  VERSION = '1.0.250329'
+  VERSION = '1.0.250330'
 
   # Colora::Error for raised exceptions in Colora.
   # Anything else is a bug.
